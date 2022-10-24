ef7c191a0df5bcb47a2acecbf07da7df6d505d1f
TJ <ctj993@gmail.com>
2022-10-24 20:58:21 +0800

    Spring Inversion of Control - XML Configuration

---
+++ demo/src/main/java/com/example/demo/CricketCoach.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class CricketCoach implements Coach {
+    private FortuneService fortuneService;
+
+    private String emailAddress;
+    private String team;
+
+    public CricketCoach(){}
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "cricket coach daily workout";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+}
